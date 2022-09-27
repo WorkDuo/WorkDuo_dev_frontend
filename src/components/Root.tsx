0b3c41ff895fe93118d5fdee8f3c42798030ee53
Guiwoo <park.guiwoo@hotmail.com>
2022-09-27 20:01:00 +0900

    header

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,25 @@
+import {Box, Button, HStack} from "@chakra-ui/react";
 import {Outlet} from "react-router-dom";
+import {GiConverseShoe} from "react-icons/gi";
 
 export default function Root() {
   return (
-    <h1>
-      I'm Root
+    <Box>
+      <HStack
+        justifyContent={"space-between"}
+        py={"5"}
+        px={"10"}
+        borderBottomWidth={2}
+      >
+        <Box color={"green.500"}>
+          <GiConverseShoe size={"48"} />
+        </Box>
+        <HStack spacing={2}>
+          <Button>Log in</Button>
+          <Button colorScheme={"green"}>Sign up</Button>
+        </HStack>
+      </HStack>
       <Outlet />
-    </h1>
+    </Box>
   );
 }
